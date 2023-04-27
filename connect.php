6ccb6142a72c12369af87998b81d09243c1d049f
loran-viscardi <103280132+loran-viscardi@users.noreply.github.com>
2023-04-27 00:10:53 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,27 +1,27 @@
 <?php
   
-	session_start();
+        session_start();
 
-	if (!isset($_SESSION['dateOPR'])){
-		$_SESSION['dateOPR'] = 0;
-	}
+        if (!isset($_SESSION['dateOPR'])){
+                $_SESSION['dateOPR'] = 0;
+        }
 
-	function Connection(){
+        function Connection(){
 //ec2-15-228-78-221.sa-east-1.compute.amazonaws.com
-		$server="....rds.amazonaws.com";   // endereço do banco de dados ou localhost para locais
-		$user="admin";  // usuario
-		$pass="*****";  // senha
-		$db="sensor";   // nome da base de dados criada em mysql>  create database sensor;
+                $server="data-base-loran.cmrb6dtspj1m.us-east-1.rds.amazonaws.com";   // endereço do banco de dados ou localhost para locais
+                $user="admin";  // usuario
+                $pass="TriangulO321!!";  // senha
+                $db="sensor";   // nome da base de dados criada em mysql>  create database sensor;
 
-		$connection = mysqli_connect($server, $user, $pass) or die("Unable to Connect to '$server'");
+                $connection = mysqli_connect($server, $user, $pass) or die("Unable to Connect to '$server'");
 
-		if (!$connection) {
-		    die('MySQL ERROR: ' . mysqli_connect_error());
-		}
+                if (!$connection) {
+                    die('MySQL ERROR: ' . mysqli_connect_error());
+                }
 
-		mysqli_select_db($connection,$db) or die( 'MySQL ERRO: '. mysqli_connect_error() );
+                mysqli_select_db($connection,$db) or die( 'MySQL ERRO: '. mysqli_connect_error() );
 
-		return $connection;
-	}
+                return $connection;
+        }
 
 ?>
